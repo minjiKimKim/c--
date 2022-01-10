6df4ea8a2c0bcff966608b05b3f8b5e2d99c5ee4
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-01-10 18:35:33 +0900

    Add files via upload

---
+++ 백준4.c
@@ -0,0 +1,14 @@
+/*백준4. 아래 예제와 같이 개를 출력하시오.*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("l\\_/l\n"); 
+	printf("lq pl\t/}\n");
+	printf("( 0 )\"\"\"\\\n"); // printf() 함수에서, 큰따옴표(쌍따옴표) 출력 방법 출력하려는 큰따옴표 바로 앞에 백슬래시(\) 기호를 하나 붙여서, 쌍따옴표를 이스케이프해주어야 합니다.
+	printf("l\"^\"`\tl\n");
+	printf("ll_/=\\\\_ _l\n");
+
+
+	return 0;
+}
