8a90f32bcb52fbc1f3e820a44c4c3cc45b78efa6
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-01-10 18:57:38 +0900

    Add files via upload

---
+++ 백준7.c
@@ -0,0 +1,17 @@
+/*백준7. 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+         첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+         첫째 줄에 A×B를 출력한다.*/
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    if (0 < A && B < 10)
+    {
+        printf("%d", A * B);
+    }
+
+    return 0;
+}
