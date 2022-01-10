9eddd97a60e1236dcb7ca3a0b0c567d63888ad2e
minjiKimKim <96872592+minjiKimKim@users.noreply.github.com>
2022-01-10 18:42:56 +0900

    Add files via upload

---
+++ 백준5.c
@@ -0,0 +1,14 @@
+/*백준5. 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+         첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+          첫째 줄에 A+B를 출력한다.*/
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1, b = 2;
+    int sum;
+    sum = a + b;
+    printf("%d\n", sum);
+
+    
+}
